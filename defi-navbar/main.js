9132fed1ddd6e9cbd61fde25eccf7d6dca3e0b59
laurinehrd <laurine.hrd@hotmail.fr>
2020-10-15 09:15:53 +0200

    slide down

--- defi-navbar/main.js
+++ defi-navbar/main.js
@@ -3,7 +3,7 @@ window.onscroll = slideDown;
 function slideDown(){
   var windowScroll = document.documentElement.scrollTop;
   console.log(windowScroll);
-  if(windowScroll > 200){
+  if(windowScroll > 100){
     document.getElementById('navbar').style.top = "0";
   }else{
     document.getElementById('navbar').style.top = "-56px";
